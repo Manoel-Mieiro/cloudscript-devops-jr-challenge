c9a21142f0a3fe30ebad3312d305be7e94a94e6c
Manoel-Mieiro <mjlmieiro@gmail.com>
2026-01-16 16:21:59 -0300

    update: adding providers for kubernetes and helm

--- providers.tf
+++ providers.tf
@@ -1,12 +1,31 @@
 terraform {
+  required_version = ">= 1.5"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = ">= 6.28.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.13"
     }
   }
-  required_version = ">=1.14"
 }
+
+provider "aws" {
+  alias  = "default"
+  region = "us-west-2"
+
+  assume_role {
+    role_arn = var.default_arn
+  }
+}
+
 
 provider "aws" {
   alias  = "EksNetworkProvisioningRole"
@@ -16,12 +35,21 @@ provider "aws" {
     role_arn = var.vpc_role_arn
   }
 }
-
 provider "aws" {
   alias  = "EksAdmin"
   region = "us-west-2"
 
   assume_role {
     role_arn = var.eks_role_arn
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
   }
 }
