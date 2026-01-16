ea5588a9c1cab8000910f8f292923a083647f1a3
Manoel-Mieiro <mjlmieiro@gmail.com>
2026-01-16 07:53:57 -0300

    update: adding EksAdmin IAM role as provider

--- providers.tf
+++ providers.tf
@@ -8,10 +8,19 @@ terraform {
 }
 
 provider "aws" {
-  alias = "EksNetworkProvisioningRole"
+  alias  = "EksNetworkProvisioningRole"
   region = "us-west-2"
 
   assume_role {
     role_arn = var.vpc_role_arn
+  }
+}
+
+provider "aws" {
+  alias  = "EksAdmin"
+  region = "us-west-2"
+
+  assume_role {
+    role_arn = var.eks_role_arn
   }
 }
