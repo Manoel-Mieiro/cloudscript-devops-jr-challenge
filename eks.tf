6379993ba91718c70fcee5bade93bd77bcf17280
Manoel-Mieiro <mjlmieiro@gmail.com>
2026-01-16 08:00:32 -0300

    update: disabling module's optional features (i.e cloudwatch and encryption_config) as well as fixing subnet_ids assignment from both public and private to the last one only. It was decided to establish a capacity standard for the managed_node_group

--- eks.tf
+++ eks.tf
@@ -5,14 +5,22 @@ module "eks" {
   kubernetes_version                       = var.eks_k8s_version
   endpoint_public_access                   = var.eks_endpoint_public_access
   enable_cluster_creator_admin_permissions = var.eks_creator_admin_permissions
+  create_cloudwatch_log_group              = var.eks_create_cloudwatch
+  encryption_config                        = var.eks_encryption_config
+  additional_security_group_ids            = [module.vpc.default_security_group_id]
+  providers = {
+    aws = aws.EksAdmin
+  }
 
-  vpc_id     = module.vpc.default_vpc_id
-  subnet_ids = merge(module.vpc.public_subnets, module.vpc.private_subnets)
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
 
   eks_managed_node_groups = {
     default = {
       ami_type       = var.eks_node_ami_type
       instance_types = var.eks_node_instance_type
+      capacity_type  = var.eks_node_capacity_type
       min_size       = var.eks_node_min_size
       max_size       = var.eks_node_max_size
       desired_size   = var.eks_node_desired_size
