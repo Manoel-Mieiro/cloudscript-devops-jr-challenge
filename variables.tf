783af4b772e82a353faa19ecf58d5f9310ff3e64
Manoel-Mieiro <mjlmieiro@gmail.com>
2026-01-16 20:09:08 -0300

    update: adding hello_world variables

--- variables.tf
+++ variables.tf
@@ -142,3 +142,36 @@ variable "helm_nginx_chart_version" {
   type = string
 }
 
+variable "helm_nginx_create_namespace" {
+  type    = bool
+  default = false
+}
+
+variable "k8s_ingress_name" {
+  type = string
+}
+
+variable "k8s_demo_path" {
+  type = string
+}
+
+variable "helm_hello_world_name" {
+  type = string
+}
+
+variable "helm_hello_world_chart" {
+  type = string
+}
+
+variable "helm_hello_world_repository" {
+  type = string
+}
+
+variable "helm_hello_world_namespace" {
+  type = string
+}
+
+variable "helm_hello_world_create_namespace" {
+  type = bool
+  default = false
+}
