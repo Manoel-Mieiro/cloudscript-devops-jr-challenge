0f4ab0243234d4b8ea2f3b59e9606b82cafaf0f4
Manoel-Mieiro <mjlmieiro@gmail.com>
2026-01-16 23:41:27 -0300

    fix: resource dependency

--- plataform/nginx.tf
+++ plataform/nginx.tf
@@ -1,3 +1,20 @@
+resource "helm_release" "ingress_nginx" {
+  name             = var.helm_nginx_name
+  chart            = var.helm_nginx_chart_name
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  namespace        = var.nginx_namespace_name
+  version          = var.helm_nginx_chart_version
+  create_namespace = var.helm_nginx_create_namespace
+
+  timeout = 300
+
+
+  set {
+    name  = "controller.service.type"
+    value = "LoadBalancer"
+  }
+}
+
 resource "kubernetes_ingress_v1" "ingress_nginx" {
   wait_for_load_balancer = true
   metadata {
@@ -25,25 +42,7 @@ resource "kubernetes_ingress_v1" "ingress_nginx" {
     }
 
   }
-  depends_on = [helm_release.hello_world]
-}
-
-resource "helm_release" "ingress_nginx" {
-  name             = var.helm_nginx_name
-  chart            = var.helm_nginx_chart_name
-  repository       = "https://kubernetes.github.io/ingress-nginx"
-  namespace        = var.nginx_namespace_name
-  version          = var.helm_nginx_chart_version
-  create_namespace = var.helm_nginx_create_namespace
-
-  timeout = 300
-
-
-  set {
-    name  = "controller.service.type"
-    value = "LoadBalancer"
-  }
-  depends_on = [kubernetes_ingress_v1.ingress_nginx]
+  depends_on = [helm_release.hello_world, helm_release.ingress_nginx]
 }
 
 
