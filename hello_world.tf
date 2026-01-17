f6285a2df76894541f8b5c04e6021d5b8b190ed4
Manoel-Mieiro <mjlmieiro@gmail.com>
2026-01-16 22:15:01 -0300

    delete: deleting files from root, following new project structure (nesting)

--- hello_world.tf
+++
@@ -1,7 +0,0 @@
-resource "helm_release" "hello_world" {
-  name             = var.helm_hello_world_name
-  namespace        = var.helm_hello_world_namespace
-  repository       = var.helm_hello_world_repository
-  chart            = var.helm_hello_world_chart
-  create_namespace = var.helm_hello_world_create_namespace
-}
